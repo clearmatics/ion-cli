695f7c24c90946c73103688feacdfcc676293310
andrea <dinennoandrea@gmail.com>
2020-03-12 14:51:23 +0000

    Fix types

--- backend/session.go
+++ backend/session.go
@@ -12,32 +12,9 @@ type Session struct {
 	// network
 	Rpc string `json:"rpc"`
 	Active bool `json:"active"`
-	Account Wallet `json:"account"`
+	AccountName string `json:"account"`
 }
 
-// TODO reads from the accounts object and store in session the one requested
-func (s *Session) ParseAccount(accountName string) error {
-
-	// retrieve private key
-	//configs, err := ioutil.ReadFile(configFile)
-	//if err != nil {
-	//	return err
-	//}
-	//
-	//userkey, err = keystore.DecryptKey(keyjson, password)
-	//if err != nil {
-	//	return nil, nil, err
-	//}
-	//
-	//// Create an authorized transactor
-	//key := ReadString(privkeystore)
-	//auth, err = bind.NewTransactor(strings.NewReader(key), password)
-	//if err != nil {
-	//	return nil, nil, err
-	//}
-
-	return nil
-}
 
 func (s *Session) PersistSession(path string) error {
 	b, err := json.Marshal(s)
