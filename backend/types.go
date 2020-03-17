4992b5a609252d616fa41e864e33c87de0e450b8
cc <siukchung@hotmail.co.uk>
2020-03-17 12:40:07 +0000

    Removed redundant type
    Signed-off-by: cc <siukchung@hotmail.co.uk>

--- backend/types.go
+++ backend/types.go
@@ -1,21 +1,20 @@
 package backend
 
 import (
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
-	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/ethereum/go-ethereum/core/types"
 )
+
 // TODO might need to specify on which consensus
 type BlockHeader struct {
-	Header *types.Header `json:"header"`
-	RlpEncoded string`json:"rlp_encoded"`
+	Header     *types.Header `json:"header"`
+	RlpEncoded string        `json:"rlp_encoded"`
 }
 
 type Transaction struct {
-	Tx *types.Transaction `json:"tx"`
-	Proof string `json:"proof"`
+	Tx    *types.Transaction `json:"tx"`
+	Proof string             `json:"proof"`
 }
 
 type Session struct {
@@ -23,31 +22,25 @@ type Session struct {
 	// lenght of the session
 
 	// network
-	Rpc string `json:"rpc"`
-	Active bool `json:"active"`
+	Rpc         string `json:"rpc"`
+	Active      bool   `json:"active"`
 	AccountName string `json:"account"`
 
 	// fields that have to be cached for subsequent calls
-	Block BlockHeader `json:"block"`
+	Block       BlockHeader `json:"block"`
 	Transaction Transaction `json:"transaction"`
-}
-
-type EthClient struct {
-	client    *ethclient.Client
-	rpcClient *rpc.Client
-	url       string
 }
 
 // an unlocked wallet object
 type Wallet struct {
 	Auth *bind.TransactOpts `json:"auth"`
-	Key  *keystore.Key `json:"key"`
-	Name string `json:"name"`
+	Key  *keystore.Key      `json:"key"`
+	Name string             `json:"name"`
 }
 
 // an account info as stored in the configs
 type AccountInfo struct {
-	Name string `json:"name"`
-	Keyfile string `json:"keyfile"`
+	Name     string `json:"name"`
+	Keyfile  string `json:"keyfile"`
 	Password string `json:"password"`
 }
